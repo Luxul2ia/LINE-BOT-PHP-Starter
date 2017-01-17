83dd8c11e1ae9f486a0ef2ae3b0178e4fc110390
l3er5erk <sengnavoy@gmail.com>
2017-01-17 17:26:52 +0700

    s
    s

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'V8rcjXnG85DSWviVZ/OFI/aolrY37Yc+/HtFrDHavdPvO18+STnDmQhR44Y3eySehRqVGSLIeGjsIXuPlceUIZoDLlIVd/nJ6DNP2wjVrr0QkwlgyPyNBvubD1cFL9s9xSPfBJVk0N8lXvAMR0C1mQdB04t89/1O/w1cDnyilFU=';
 $proxy = 'http://fixie:7Q97GZEo7k6U6iI@velodrome.usefixie.com:80';
-$proxyauth = 'http://fixie:7Q97GZEo7k6U6iI@velodrome.usefixie.com:80;
+$proxyauth = '54.173.229.200;
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
