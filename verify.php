464dfa38807d835720bc88d4b5c9ddd118c181fd
l3er5erk <sengnavoy@gmail.com>
2017-01-17 17:30:01 +0700

    dsadsa
    dsad

--- verify.php
+++ verify.php
@@ -1,6 +1,8 @@
 <?php
 $access_token = 'V8rcjXnG85DSWviVZ/OFI/aolrY37Yc+/HtFrDHavdPvO18+STnDmQhR44Y3eySehRqVGSLIeGjsIXuPlceUIZoDLlIVd/nJ6DNP2wjVrr0QkwlgyPyNBvubD1cFL9s9xSPfBJVk0N8lXvAMR0C1mQdB04t89/1O/w1cDnyilFU=';
+
 $url = 'https://api.line.me/v1/oauth/verify';
+
 $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
